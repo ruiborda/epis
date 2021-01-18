6e83f4b9f7a8e822c0a8bcea30242aa83ff53e86
RuiBorda <bordarui@gmail.com>
2021-01-18 00:18:30 -0500

    se removió la selección de tipo de usuario, se agregó aliniamiento vertical, traducción al español

--- frontend/src/routes/intranet-del-programa/index.svelte
+++ frontend/src/routes/intranet-del-programa/index.svelte
@@ -1,94 +1,74 @@
 <script context="module">
-
 </script>
 
 <script>
-    
 </script>
 
-<style>
-    
-</style>
-
 <svelte:head>
-    <title> Login</title>
-    <meta name="" content="">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/journal/bootstrap.min.css" integrity="sha384-QDSPDoVOoSWz2ypaRUidLmLYl4RyoBWI44iA5agn6jHegBxZkNqgm2eHb6yZ5bYs" crossorigin="anonymous">
-    <script defer src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" integrity="sha384-9/D4ECZvKMVEJ9Bhr3ZnUAF+Ahlagp1cyPC7h5yDlZdXs4DQ/vRftzfd+2uFUuqS" crossorigin="anonymous">
-    </script>
+  <title>Login</title>
+  <meta name="" content="" />
 </svelte:head>
 
-<!--
-<div class="row">
-    <div class="col-md-8">
-        <form>
-            <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
-                    else.</small>
-            </div>
-            <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
-            </div>
-            <div class="form-group form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-    </div>
-</div>
--->
-
-
-<div class="container h-100">
-
-    <form>
-        <div class="input-group d-flex justify-content-center">
-            <select name="perfil">
-            <option value="1">Docente</option> 
-            <option value="2">Director</option> 
-            <option value="3">Administrativo</option>
-
-            </select>
+<div class="container vh-100">
+  <div class="row align-items-center vh-100">
+    <div class="col">
+      <form>
+        <div class="input-group mb-3">
+          <div class="input-group-append">
+            <span class="input-group-text"><i class="fas fa-user" /></span>
+          </div>
+          <input
+            type="text"
+            name=""
+            class="form-control input_user"
+            value=""
+            placeholder="Nombre de usuario"
+          />
         </div>
-        <br>
-            <div class="input-group mb-3">
-                <div class="input-group-append">
-                    <span class="input-group-text"><i class="fas fa-user"></i></span>
-                </div>
-                <input type="text" name="" class="form-control input_user" value="" placeholder="username">
-            </div>
 
-            <div class="input-group mb-3">
-                <div class="input-group-append">
-                <span class="input-group-text"><i class="fas fa-key"></i></span>
-                </div>
-                <input type="password" name="" class="form-control input_pass" value="" placeholder="password"> 
-            </div>
+        <div class="input-group mb-3">
+          <div class="input-group-append">
+            <span class="input-group-text"><i class="fas fa-key" /></span>
+          </div>
+          <input
+            type="password"
+            name=""
+            class="form-control input_pass"
+            value=""
+            placeholder="Contraseña"
+          />
+        </div>
 
         <div class="form-group">
-            <div class="custom-control custom-checkbox">
-                <input type="checkbox" class="custom-control-input" id="customControlInline">
-                <label class="custom-control-label" for="customControlInline">Remember me</label>
-            </div>
+          <div class="custom-control custom-checkbox">
+            <input
+              type="checkbox"
+              class="custom-control-input"
+              id="customControlInline"
+            />
+            <label class="custom-control-label" for="customControlInline"
+              >Recuérdame</label
+            >
+          </div>
         </div>
 
         <div class="d-flex justify-content-center mt-3 login_container">
-            <button type="button" name="button" class="btn login_btn bg-secondary">Login</button>
+          <button type="button" name="button" class="btn login_btn bg-secondary"
+            >Iniciar sesión</button
+          >
         </div>
-
       </form>
-
-</div>
-<div class="mt-4">
-    <div class="d-flex justify-content-center links">
-        ¿No tienes una cuenta? <a href="#" class="ml-2">Regístrate</a>
+      <div class="mt-4">
+        <div class="d-flex justify-content-center links">
+          ¿No tienes una cuenta? <a href="." class="ml-2">Regístrate</a>
+        </div>
+        <div class="d-flex justify-content-center links">
+          <a href=".">Olvidaste tu contraseña</a>
+        </div>
+      </div>
     </div>
-    <div class="d-flex justify-content-center links">
-        <a href="#">Olvidaste tu contraseña</a>
-    </div>
+  </div>
 </div>
 
+<style>
+</style>
