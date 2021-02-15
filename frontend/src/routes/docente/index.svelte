a6b767df3932f8e34b6f39dae7289c39e8c38a4e
KevinMarcas <kevinmarcas3@gmail.com>
2021-02-15 16:39:47 -0500

    lista de docentes

--- frontend/src/routes/docente/index.svelte
+++ frontend/src/routes/docente/index.svelte
@@ -1,1 +1,47 @@
-<h1>hola mundo</h1>
+<script>
+import Footer from "../../components/inicio/Footer.svelte";
+
+</script>
+
+<svelte:head>
+    <title>EPIS: Docentes</title>
+    <meta name="Description" content="160 caracteres Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid at cumque, ex odio officia porro quas quibusdam soluta voluptatem. Accusantium">
+</svelte:head>
+
+
+<div class="cotainer">
+    <div class="row">
+        <div class="col">
+
+            <ul class="list-unstyled">
+                <li class="media">
+                  <img src="..." class="mr-3" alt="...">
+                  <div class="media-body">
+                    <h5 class="mt-0 mb-1">List-based media object</h5>
+                    <p>All my girls vintage Chanel baby. So you can have your cake. Tonight, tonight, tonight, I'm walking on air. Slowly swallowing down my fear, yeah yeah. Growing fast into a bolt of lightning. So hot and heavy, 'Til dawn. That fairy tale ending with a knight in shining armor. Heavy is the head that wears the crown.</p>
+                  </div>
+                </li>
+                <li class="media my-4">
+                  <img src="..." class="mr-3" alt="...">
+                  <div class="media-body">
+                    <h5 class="mt-0 mb-1">List-based media object</h5>
+                    <p>Maybe a reason why all the doors are closed. Cause once you’re mine, once you’re mine. Be your teenage dream tonight. Heavy is the head that wears the crown. It's not even a holiday, nothing to celebrate. A perfect storm, perfect storm.</p>
+                  </div>
+                </li>
+                <li class="media">
+                  <img src="..." class="mr-3" alt="...">
+                  <div class="media-body">
+                    <h5 class="mt-0 mb-1">List-based media object</h5>
+                    <p>Are you brave enough to let me see your peacock? There’s no going back. This is the last time you say, after the last line you break. At the eh-end of it all.</p>
+                  </div>
+                </li>
+              </ul>
+
+        </div>
+    </div>
+
+
+</div>
+
+
+<Footer />
