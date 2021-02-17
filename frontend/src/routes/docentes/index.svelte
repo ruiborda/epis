eb5bad5980349ab1237519e629f8fabad78c5bb1
KevinMarcas <kevinmarcas3@gmail.com>
2021-02-16 23:37:26 -0500

    añadiendo enlace docentes/sección docentes

---
+++ frontend/src/routes/docentes/index.svelte
@@ -0,0 +1,47 @@
+<script>
+import Footer from "../../components/inicio/Footer.svelte";
+
+</script>
+
+
+<svelte:head>
+  <title>Docentes:: EPIS</title>
+  <meta name="" content="" />
+</svelte:head>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+
+            <ul class="list-unstyled">
+                <li class="media">
+                  <img src="http://directorio.concytec.gob.pe/appDirectorioCTI/UploadFotoPath.do?tipo=visualizar_archivo&id_investigador=76078&ruta=/documents/docInvestigadores/76078/imagenes/JUAN%20JOSE%20ORE%20CERRON.jpg&content_type=image/jpeg" width="100" height="100" class="mr-3" alt="...">
+                  <div class="media-body">
+                    <h5 class="mt-0 mb-1">List-based media object</h5>
+                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                  </div>
+                </li>
+                <li class="media my-4">
+                  <img src="http://directorio.concytec.gob.pe/appDirectorioCTI/UploadFotoPath.do?tipo=visualizar_archivo&id_investigador=76078&ruta=/documents/docInvestigadores/76078/imagenes/JUAN%20JOSE%20ORE%20CERRON.jpg&content_type=image/jpeg" width="100" height="100" class="mr-3" alt="...">
+                  <div class="media-body">
+                    <h5 class="mt-0 mb-1">List-based media object</h5>
+                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                  </div>
+                </li>
+                <li class="media">
+                  <img src="..." width="100" height="100" class="mr-3" alt="...">
+                  <div class="media-body">
+                    <h5 class="mt-0 mb-1">List-based media object</h5>
+                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                  </div>
+                </li>
+              </ul>
+
+
+        </div>
+    </div>
+</div>
+
+
+
+<Footer />
