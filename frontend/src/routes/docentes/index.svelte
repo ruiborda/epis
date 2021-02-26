5ba02a77697ff62b91313e1d9ab23e9a2bb3f38f
KevinMarcas <kevinmarcas3@gmail.com>
2021-02-26 10:41:51 -0500

    se añadio sección docente(lista) y informacion-docente

--- frontend/src/routes/docentes/index.svelte
+++ frontend/src/routes/docentes/index.svelte
@@ -1,37 +1,51 @@
 <script>
 import Footer from "../../components/inicio/Footer.svelte";
 
+
 </script>
 
 
 <svelte:head>
   <title>Docentes:: EPIS</title>
   <meta name="" content="" />
+
 </svelte:head>
 
 <div class="container">
-    <div class="row">
+  <div class="row border m-3">
+    <div class="col" align="center">
+      <h5>Lista de Profesores</h5>
+      <p>25 de febrero de 2021 <br> Periodo academico: 2021-01</p>
+
+    </div>
+  </div>
+</div>
+
+<div class="container">
+    <div class="row border m-3">
         <div class="col">
 
             <ul class="list-unstyled">
                 <li class="media">
                   <img src="http://directorio.concytec.gob.pe/appDirectorioCTI/UploadFotoPath.do?tipo=visualizar_archivo&id_investigador=76078&ruta=/documents/docInvestigadores/76078/imagenes/JUAN%20JOSE%20ORE%20CERRON.jpg&content_type=image/jpeg" width="100" height="100" class="mr-3" alt="...">
                   <div class="media-body">
-                    <h5 class="mt-0 mb-1">List-based media object</h5>
+                    <h5 class="mt-0 mb-1"><a href="docentes/informacion-docente"> List-based media object </a></h5>
                     Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
                   </div>
                 </li>
+                <hr>
                 <li class="media my-4">
                   <img src="http://directorio.concytec.gob.pe/appDirectorioCTI/UploadFotoPath.do?tipo=visualizar_archivo&id_investigador=76078&ruta=/documents/docInvestigadores/76078/imagenes/JUAN%20JOSE%20ORE%20CERRON.jpg&content_type=image/jpeg" width="100" height="100" class="mr-3" alt="...">
                   <div class="media-body">
-                    <h5 class="mt-0 mb-1">List-based media object</h5>
+                    <h5 class="mt-0 mb-1"><a href="docentes/informacion-docente"> List-based media object </a></h5>
                     Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
                   </div>
                 </li>
+                <hr>
                 <li class="media">
-                  <img src="..." width="100" height="100" class="mr-3" alt="...">
+                  <img src="http://directorio.concytec.gob.pe/appDirectorioCTI/UploadFotoPath.do?tipo=visualizar_archivo&id_investigador=76078&ruta=/documents/docInvestigadores/76078/imagenes/JUAN%20JOSE%20ORE%20CERRON.jpg&content_type=image/jpeg" width="100" height="100" class="mr-3" alt="...">
                   <div class="media-body">
-                    <h5 class="mt-0 mb-1">List-based media object</h5>
+                    <h5 class="mt-0 mb-1"><a href="docentes/informacion-docente"> List-based media object </a></h5>
                     Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
                   </div>
                 </li>
---
+++ frontend/src/routes/docentes/informacion-docente.svelte
@@ -0,0 +1,167 @@
+<script>
+
+</script>
+
+<svelte:head>
+    <title>Informacion: Docente</title>
+    <meta name="" content=""/>
+</svelte:head>
+
+<div class="container"> 
+    <div class="panel panel-primary" style="border-color: #5dc1d4;">
+        <div class="panel-heading" style="border-color: #5dc1d4; color: white; border-top-left-radius: 0px !important; border-top-right-radius: 0px !important; background-color: #5dc1d4;">
+            <a id="Home" href="/docentes"><i class="fa fa-home fa-lg" style="color:white"></i></a>
+    </div>
+
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-md-3" id="img" style="text-align:center">
+                    <img id="" src="http://directorio.concytec.gob.pe/appDirectorioCTI/UploadFotoPath.do?tipo=visualizar_archivo&id_investigador=76078&ruta=/documents/docInvestigadores/76078/imagenes/JUAN%20JOSE%20ORE%20CERRON.jpg&content_type=image/jpeg" style="width: 100% heigth=100%;" alt="...">
+                </div>
+                
+                <div class="col-md-9">
+    
+                    <table>
+                        <tbody>
+                        <tr>
+                            <td>
+                                <span><strong>NOMBRE</strong>: </span>
+                            </td>
+                            <td>
+                                <span id="ctl00_ctl49_g_a2eb7972_7342_4603_a4d7_3b894d0688f7_ctl00_NOMBRE" style="font-size: 14px;">Abarca Rubianes, Carlos Rodrigo</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <span><strong>CLASE</strong>: </span>
+                            </td>
+                            <td>
+                                <span id="ctl00_ctl49_g_a2eb7972_7342_4603_a4d7_3b894d0688f7_ctl00_CLASE" style="font-size: 14px;">CONTRATADO</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <span><strong>CATEGORÍA</strong>: </span>
+                            </td>
+                            <td>
+                                <span id="ctl00_ctl49_g_a2eb7972_7342_4603_a4d7_3b894d0688f7_ctl00_CATEGORIA" style="font-size: 14px;">NO ORDINARIZADO</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <span><strong>LABOR</strong>: </span>
+                            </td>
+                            <td>
+                                <span id="ctl00_ctl49_g_a2eb7972_7342_4603_a4d7_3b894d0688f7_ctl00_LABOR" style="font-size: 14px;">ASISTENTE DE CÁTEDRA</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <span><strong>NIVEL EN EL QUE ENSEÑA</strong>: </span>
+                            </td>
+                            <td>
+                                <span id="ctl00_ctl49_g_a2eb7972_7342_4603_a4d7_3b894d0688f7_ctl00_NIVEL" style="font-size: 14px;">PRE-GRADO</span>
+                            </td>
+                        </tr>
+                    </tbody></table>
+    
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="notice notice-info" style="background-color: #5dc1d4;">
+        <strong>GRADOS OFICIALES</strong>
+    </div>
+
+    
+    <table class="table" id="table#" style="font-size: 12px;">
+        <thead>
+            <tr>
+                <th>GRADO ACADÉMICO</th>
+                <th>TÍTULO</th>
+                <th>INSTITUCIÓN</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td>GRADO DE MAESTRIA </td>
+                <td>MÁSTER PROPIO EN DERECHO DEL TRABAJO Y DE LA SEGUR </td>
+                <td>UNIVERSIDAD DE VALENCIA </td>
+            </tr>
+
+            <tr>
+                <td>GRADO DE BACHILLER </td>
+                <td>DERECHO </td>
+                <td>UNIVERSIDAD CATÓLICA SAN PABLO </td>
+            </tr>
+        </tbody>
+    </table>
+
+    <div id="#">
+        <div class="notice notice-info" style="background-color: #5dc1d4;">
+            <strong>EXPERIENCIA EDUCATIVA</strong>
+        </div>
+
+        <table class="table" id="#" style="font-size:12px">
+            <thead>
+                <tr>
+                    <th width="33%">ESCUELA</th>
+                    <th width="33%">CURSO</th>
+                    <th>PERIODO</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Ingenieria de sistemas</td>
+                    <td>Diseño Web</td>
+                    <td>2020-2</td>
+                </tr>
+                <tr>
+                    <td>Ingenieria de sistemas</td>
+                    <td>Diseño y Estructura de datos</td>
+                    <td>2020-2</td>
+                </tr>
+                <tr>
+                    <td>Ingenieria de sistemas</td>
+                    <td>Programación orientada a objetos</td>
+                    <td>2020-1</td>
+                </tr>
+                <tr>
+                    <td>Ingenieria de sistemas</td>
+                    <td>Sistemas Operativos</td>
+                    <td>2019-2</td>
+                </tr>
+            </tbody>
+
+        </table>
+
+
+    </div>
+
+    <div id="#">
+        <div class="notice notice-info"  style="background-color: #5dc1d4;">
+            <strong>TRAYECTORIA PROFESIONAL</strong>
+        </div>
+        <table class="table" id="table#" style="font-size: 12px;">
+            <thead>
+                <tr>
+                    <th width="50%"> EMPRESA</th>
+                    <th>PUESTO</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>UNIVERSIDAD JOSÉ MARÍA ARGUEDAS</td>
+                    <td>DOCENTE</td>
+                </tr>
+                <tr>
+                    <td>MUNICIPALIDAD ANDAHUAYLAS</td>
+                    <td>DIRECTOR DE AREA DE TI</td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
+</div>
