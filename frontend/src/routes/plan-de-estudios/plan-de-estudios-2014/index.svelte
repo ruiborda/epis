eaae20029989cccc48fbf00c6dca413e5035b8d8
KevinMarcas <kevinmarcas3@gmail.com>
2021-03-02 23:06:09 -0500

    embebido de ficheros en la seccion de plan de estudios

---
+++ frontend/src/routes/plan-de-estudios/plan-de-estudios-2014/index.svelte
@@ -0,0 +1,18 @@
+<script>
+    
+</script>
+
+<svelte:head>
+    <title>Plan de estudios 2014 :: EPIS</title>
+    <meta name="" content="" />
+</svelte:head>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+
+            <embed src="http://epis.unajma.edu.pe/sites/default/files/malla_epis_2014.pdf" type="application/pdf" width="100%" height="600px" />
+
+        </div>
+    </div>
+</div>
