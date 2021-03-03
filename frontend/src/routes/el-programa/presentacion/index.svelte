6cd640f5cadd089f5d4135a0cacc2099be18ed0b
KevinMarcas <kevinmarcas3@gmail.com>
2021-03-02 23:04:55 -0500

    actualización seccion el programa>presentación

---
+++ frontend/src/routes/el-programa/presentacion/index.svelte
@@ -0,0 +1,28 @@
+<script>
+import Footer from "../../../components/inicio/Footer.svelte";
+
+
+</script>
+
+<svelte:head>
+    <title>Presentación :: EPIS</title>
+</svelte:head>
+
+<br><br>
+<div class="container">
+    <div class="row">
+        <div class="col" style="text-align:justify">
+
+            <p>
+                Formamos profesionales orientados a la creación de nuevas tecnologías computacionales que permitan impulsar la 
+                industria del software, capaces de diseñar y desarrollar desde infraestructura de plataformas como sistemas operativos,
+                programas de comunicación, entre otros, hasta aplicaciones de tecnologías como navegadores de Internet, bases de datos,
+                motores de búsqueda, etc.
+
+            </p>
+
+        </div>
+    </div>
+</div>
+
+<Footer />
